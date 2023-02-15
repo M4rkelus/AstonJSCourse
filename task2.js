78a924eee508c6aa14b3bb96f3f420ef5b14790c
Mark Pirogov <m4rkpirogov@gmail.com>
2023-02-15 19:40:11 +0300

    fix: removed unnecessary consoles, divided into two files
    Signed-off-by: Mark Pirogov <m4rkpirogov@gmail.com>

---
+++ task2.js
@@ -0,0 +1,14 @@
+export const getNumberRadix = (number, radix) => {
+  try {
+    if (isNaN(number) || number < 0 || isNaN(radix) || radix < 2 || radix > 16) {
+      throw new Error('Функция getNumberRadix была вызвана c некорректными параметрами.');
+    }
+    if (typeof number === 'string') {
+      number = parseInt(number, 10);
+    }
+
+    return number.toString(radix);
+  } catch (error) {
+    return error.message;
+  }
+}
